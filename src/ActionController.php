2a4b04b0d479dd0bfa1f8ac904357f85108dd06f
Humberto <jairhumberto@gmail.com>
2017-12-05 14:22:59 -0200

    Moved for a src directory to comply with composer structure

---
+++ src/ActionController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Squille PHP Framework (http://squille.com/squille-php-framework)
+ *
+ * @copyright Copyright (c) 2017 Squille
+ * @license   this software is distributed under MIT license, see the license.mit file
+ */
+
+
+namespace Squille\Core;
+
+abstract class ActionController {
+    private $viewFactory;
+
+    // TODO: seguir modelo de convenção na criação das views.
+    public function __construct(ViewFactory $viewFactory) {
+        $this->viewFactory = $viewFactory;
+    }
+
+    public function getViewFactory() {
+        return $this->viewFactory;
+    }
+}
---
+++ src/Collection.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Squille PHP Framework (http://squille.com/squille-php-framework)
+ *
+ * @copyright Copyright (c) 2017 Squille
+ * @license   this software is distributed under MIT license, see the license.mit file
+ */
+
+namespace Squille\Core;
+
+class Collection extends \ArrayIterator {
+
+    public function add($index, $value) {
+        $this->offsetSet($index, $value);
+    }
+
+    public function get($index) {
+        return $this->offsetGet($index);
+    }
+
+    public function getFirst() {
+        foreach ($this as $value) {
+            return $value;
+        }
+    }
+
+    public function getLast() {
+        foreach ($this as $value) {
+            $r = $value;
+        }
+        
+        return $r;
+    }
+
+    public function valueExists($needle) {
+        foreach ($this as $key => $value) {
+            if ($needle == $value) {
+                return $key;
+            }
+        }
+        
+        return false;
+    }
+
+    public function keyExists($needle) {
+        foreach ($this as $key => $value) {
+            if ($needle == $key) {
+                return $value;
+            }
+        }
+        
+        return false;
+    }
+}
